c8cbda5c0844af2f1cdfaaeb725003d0c55fa391
Stephen Haxby <stephenhaxby@stephens-mini.gateway>
2017-08-11 06:28:53 +1000

    Hooking up data to the Edit Commute screen...

--- AverageMyCommute/CommuteViewController.swift
+++ AverageMyCommute/CommuteViewController.swift
@@ -7,7 +7,112 @@
 //
 
 import UIKit
+import CoreData
 
 class CommuteViewController : UIViewController {
     
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var startTimeDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var endTimeDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var monSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var tueSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var wedSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var thurSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var friSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var satSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var sunSegmentControl: UISegmentedControl!
+    
+    enum buttonTitles : String {
+        
+        case Add = "Add"
+        case Edit = "Edit"
+    }
+    
+    var appDelegate : AppDelegate {
+        
+        get
+        {
+            
+            return (UIApplication.shared.delegate as! AppDelegate)
+        }
+    }
+    
+    var commute : Commute?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        if let commuteToEdit = commute {
+            
+            addButton.title = buttonTitles.Edit.rawValue
+            view(commute: commuteToEdit)
+        }
+        else {
+            
+            addButton.title = buttonTitles.Add.rawValue
+            viewDefaultCommute()
+        }
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        let commute =
+            NSEntityDescription.insertNewObject(forEntityName: "Commute", into: appDelegate.coreDataContext) as! Commute
+        
+        commute.id = UUID().uuidString
+        commute.timeStart = startTimeDatePicker.date as NSDate
+        commute.timeEnd = endTimeDatePicker.date as NSDate
+        commute.monday = monSegmentControl.selectedSegmentIndex == 0
+        commute.tuesday = tueSegmentControl.selectedSegmentIndex == 0
+        commute.wednesday = wedSegmentControl.selectedSegmentIndex == 0
+        commute.thursday = thurSegmentControl.selectedSegmentIndex == 0
+        commute.friday = friSegmentControl.selectedSegmentIndex == 0
+        commute.saturday = satSegmentControl.selectedSegmentIndex == 0
+        commute.sunday = sunSegmentControl.selectedSegmentIndex == 0
+    }
+    
+    func viewDefaultCommute() {
+        
+        startTimeDatePicker.date = Date()
+        endTimeDatePicker.date = Date()
+        
+        monSegmentControl.selectedSegmentIndex = 0
+        tueSegmentControl.selectedSegmentIndex = 0
+        wedSegmentControl.selectedSegmentIndex = 0
+        thurSegmentControl.selectedSegmentIndex = 0
+        friSegmentControl.selectedSegmentIndex = 0
+        satSegmentControl.selectedSegmentIndex = 0
+        sunSegmentControl.selectedSegmentIndex = 0
+    }
+    
+    func view(commute : Commute?) {
+        
+        if let editCommute : Commute = commute {
+            
+            startTimeDatePicker.date = editCommute.timeStart == nil ? Date() : editCommute.timeStart! as Date
+            endTimeDatePicker.date = editCommute.timeEnd == nil ? Date() : editCommute.timeEnd! as Date
+            
+            monSegmentControl.selectedSegmentIndex = editCommute.monday ? 0 : 1
+            tueSegmentControl.selectedSegmentIndex = editCommute.tuesday ? 0 : 1
+            wedSegmentControl.selectedSegmentIndex = editCommute.wednesday ? 0 : 1
+            thurSegmentControl.selectedSegmentIndex = editCommute.thursday ? 0 : 1
+            friSegmentControl.selectedSegmentIndex = editCommute.friday ? 0 : 1
+            satSegmentControl.selectedSegmentIndex = editCommute.saturday ? 0 : 1
+            sunSegmentControl.selectedSegmentIndex = editCommute.sunday ? 0 : 1
+        }
+        else {
+            
+            viewDefaultCommute()
+        }
+    }
 }
